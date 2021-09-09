acf3f0780db7ff46d1ae5f634bdfcd0e66db07c7
96sMicks <melkiasae@hotmail.com>
2021-09-09 18:16:10 +0000

    Refactored tweet display after mentor code review

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,21 +4,35 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const hardCodeTweet = {
-  user: {
-    name: "Newton",
-    avatars: "https://i.imgur.com/73hZDYK.png",
-    handle: "@SirIsaac",
+const hardCodeTweet = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
   },
-  content: {
-    text: "If I have seen further it is by standing on the shoulders of giants",
-  },
-  created_at: 1461116232227,
-};
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+
 
 $(document).ready(function() {
   const createTweetElement = function (obj) {
-  const $tweet = $(`<article>
+  const $tweet = $(`<article class="tweet">
                 <header class="tweet-header">
                   <div>
                     <img src="${obj.user.avatars}"></i>
@@ -49,9 +63,25 @@ $(document).ready(function() {
   return $tweet;
 };
 
-const $tweet = createTweetElement(hardCodeTweet);
+const renderTweets = function(tweetsObj) {
 
-console.log($tweet);
+  // loops through tweets
+    for (const tweet of tweetsObj) {
+      // console.log(tweet)
+      // calls createTweetElement for each tweet
+      const $tweet = createTweetElement(tweet)
 
-$(".tweet-display").append($tweet);
-})
+      
+      // takes return value and appends it to the tweets container
+      $("#tweets-container").append($tweet);
+    }
+    
+  }
+
+  renderTweets(hardCodeTweet)
+});
+
+
+// const $tweet = createTweetElement(hardCodeTweet);
+// console.log($tweet);
+
